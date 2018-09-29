8bb277b02eacc038a60da975037fbb1b65d3a666
MarlenaHovs <marlenahovsepyan@hotmail.com>
2018-09-29 13:59:52 +0400

    added HW1

---
+++ MH_HW1.py
@@ -0,0 +1,80 @@
+
+# coding: utf-8
+
+# # Variables
+
+# In[ ]:
+
+
+project = 'cake'
+difficulty = 3
+
+
+# In[2]:
+
+
+ingredients = ["flour", "butter", "sugar", "eggs", "cocoa powder", "baking powder"]
+print (ingredients)
+
+
+# In[3]:
+
+
+print ('apples' in ingredients)
+
+
+# In[4]:
+
+
+print ('butter' in ingredients)
+
+
+# In[6]:
+
+
+print ('eggs' or 'margarine' in ingredients)
+
+
+# In[8]:
+
+
+print ('eggs' and 'margarine' in ingredients)
+
+
+# In[11]:
+
+
+flour, butter,sugar, eggs, cocoa_powder, baking_powder = 175, 175, '100g', 2, '1ts', 0.5
+print (flour, butter, sugar, eggs, cocoa_powder, baking_powder)
+
+
+# In[12]:
+
+
+print (flour)
+
+
+# In[14]:
+
+
+flour = 175
+print ("flour - " + str(flour)
+
+
+# # Operators
+
+# In[15]:
+
+
+a = 15
+b = 8
+c = 2
+
+5*a**2 - a*b -a%2 - a/5
+
+
+# In[16]:
+
+
+b**3 +3*a*b - 10*c
+
